6c4ce2e59098790a4970f6689c078ef50d5ed771
1Lysy <shostak.business@gmail.com>
2026-02-03 16:34:15 +0100

    first_exercise

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-print('Hello world')
+from datetime import datetime
 
-a = 2
-b = 2
 
-a = 10
+def get_days_from_today(date):
+    
+    today = datetime.today().date()
+    try:
+        str_to_date = datetime.strptime(date, '%Y-%m-%d').date()
+
+        difference = (today - str_to_date).days
+        return difference
+    except ValueError:
+        print('Формат дати не співпадає! Потрібний формат: "РРРР-ММ-ДД"')
+        
+result = get_days_from_today('2021-10-09')
+print('Різниця в днях: ', result)
